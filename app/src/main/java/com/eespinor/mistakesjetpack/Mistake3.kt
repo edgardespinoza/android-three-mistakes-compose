3a380a01ceab54bbd57599e95a6cc7ab4dcbc59d
eespinor <edgard.espinoza.rivas@gmail.com>
2023-08-26 22:06:39 -0500

    resolver thrid mistake

--- app/src/main/java/com/eespinor/mistakesjetpack/Mistake3.kt
+++ app/src/main/java/com/eespinor/mistakesjetpack/Mistake3.kt
@@ -2,6 +2,7 @@ package com.eespinor.mistakesjetpack
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavController
 
 @Composable
@@ -9,9 +10,10 @@ fun LoginScreen(
     isLoggedIn: Boolean,
     navController:NavController
 ) {
-
-    if (isLoggedIn){
-        navController.navigate("main_screen")
+    LaunchedEffect(key1=isLoggedIn){
+        if (isLoggedIn){
+            navController.navigate("main_screen")
+        }
     }
     Box {
         //content
